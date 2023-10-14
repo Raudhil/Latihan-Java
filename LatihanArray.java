7b9263d8dd3d5d7a6f3c65f8ea90dce0a490ce43
Raudhil <raudhilfirdausn@gmail.com>
2023-10-14 22:47:16 +0700

    Latihan Penjumlahan 2 Array

---
+++ LatihanArray.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class LatihanArray{
+    public static void main(String[] args) {
+        System.out.println("Menjumlahkan array");
+
+        int array[] = {12,64,123,25,5};
+        int array2[] = {15,23,45,23,12};
+
+        Arrays.sort(array);
+        Arrays.sort(array2);
+
+        printArray(array,"Array1");
+        printArray(array2,"Array2");
+
+        hasil(array,array2);
+
+        System.out.println(Arrays.toString(hasil(array,array2)));
+
+
+    }
+    static void printArray(int [] array,String message){
+        System.out.println("Array " + message + " adalah: " + Arrays.toString(array) );
+    }
+    static int [] hasil(int [] array,int[] array2){
+        int []hasilArray = new int[array.length];
+
+        for (int i = 0;i < array.length; i++){
+            hasilArray[i] = array[i] + array2[i];
+        }
+        return hasilArray;
+    }
+}
